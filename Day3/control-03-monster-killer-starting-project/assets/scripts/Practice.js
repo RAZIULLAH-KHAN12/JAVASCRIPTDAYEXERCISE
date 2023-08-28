cd8995b0124cac8956eb007d5ce13446cf747fe6
RAZIULLAH-KHAN12 <raziullah.k@prominentpixel>
2023-08-28 10:25:34 +0530

    Day 3 Game and patter

---
+++ Day3/control-03-monster-killer-starting-project/assets/scripts/Practice.js
@@ -0,0 +1,23 @@
+// let randomNumber = [];
+// let finished = false;
+// while(!finished){
+//     const rndNumber = Math.random();
+//     randomNumber.push(rndNumber);
+//     if(rndNumber > 0.5){finished = true; 
+//         console.log(randomNumber)
+//     }
+// }
+
+// let a = 0;
+// do{
+//     console.log(a);
+//     a++;
+// }while(a<3);
+
+//continue keyword
+// for(let i = 0; i<10; i++){
+//     if(i===3){
+//         continue;
+//     }
+//     console.log(i);
+// }
---
+++ Day3/patter.js
@@ -0,0 +1,55 @@
+// patten
+
+// ooooooooooooooooo                                                       
+// ooooooooooooooooo
+// ooooooooooooooooo                                                       
+// oooo                                                                    
+// oooo                                                                    
+// oooo
+// ooooooooooooooooo                                                       
+// ooooooooooooooooo                                                       
+// ooooooooooooooooo                                                       
+//       oooo
+//       oooo
+//       oooo
+// ooooooooooooooooo
+// ooooooooooooooooo                                                       
+// ooooooooooooooooo    
+
+for (let i = 1; i <= 3; i++) {
+    for(let j = 1; j<=17; j++){
+        process.stdout.write("o");
+    }
+    console.log();
+}
+
+for (let i = 1; i <=3; i++) {
+    for(let j = 1; j<=4; j++){
+        process.stdout.write("o");
+    }
+    console.log();
+}
+
+for (let i = 1; i <= 3; i++) {
+    for(let j = 1; j<=17; j++){
+        process.stdout.write("o");
+    }
+    console.log();
+}
+
+for (let i = 1; i <=3; i++) {
+    for(let k = 1; k<=6; k++){
+        process.stdout.write(" ");
+    }
+    for(let j = 1; j<=4; j++){
+        process.stdout.write("o");
+    }
+    console.log();
+}
+
+for (let i = 1; i <= 3; i++) {
+    for(let j = 1; j<=17; j++){
+        process.stdout.write("o");
+    }
+    console.log();
+}
