5f54887e8eb0f851d51d3d72cbd3d9a262aa055d
RAZIULLAH-KHAN12 <raziullah.k@prominentpixel>
2023-10-11 13:07:28 +0530

    18 - Working with Http Requests is completed

---
+++ Day11/18_http-01-starting-setup/assets/scripts/exercise.js
@@ -0,0 +1,9 @@
+/*1) Create a staic JSON and perform crud operations using Http Requests */  
+    let data = [
+        {id: 1, name: 'Ram'},
+        {id: 2, name: 'Shyam'},
+        {id: 3, name: 'Rahul'},
+    ];
+
+
+/*2) generate error in code and catch it and also give alert about it to user */       
